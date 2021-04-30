b61c27cd3e0a3da9a4026f7e27e9ebfb1a3a65f6
Kevin Locke <kevin@kevinlocke.name>
2021-04-30 08:49:34 -0600

    Split cli from bin/cmd
    Node.js does not currently provide an equivalent to `require.main` for
    checking if a module was invoked directly.  After reading the
    `import.meta.main` discussion in nodejs/modules#274, rather than making
    behavior conditional on how the module is imported, I think it makes
    more sense for the bin script to unconditionally invoke the entry point
    function and not contain any other code.
    
    Therefore, move the code providing the entry point function and any
    other CLI functionality to its own module named cli.js and leave only
    the unconditional invocation in bin/cmd.js.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

---
+++ cli.js
@@ -0,0 +1,138 @@
+/**
+ * @copyright Copyright 2017-2020 Kevin Locke <kevin@kevinlocke.name>
+ * @license MIT
+ * @module modulename/cli.js
+ */
+
+'use strict';
+
+const Yargs = require('yargs/yargs');
+const packageJson = require('./package.json');
+const modulename = require('.');
+
+/** Options for command entry points.
+ *
+ * @typedef {{
+ *   env: !object<string,string>,
+ *   stdin: !module:stream.Readable,
+ *   stdout: !module:stream.Writable,
+ *   stderr: !module:stream.Writable
+ * }} CommandOptions
+ * @property {!object<string,string>} env Environment variables.
+ * @property {!module:stream.Readable} stdin Stream from which input is read.
+ * @property {!module:stream.Writable} stdout Stream to which output is
+ * written.
+ * @property {!module:stream.Writable} stderr Stream to which errors and
+ * non-output status messages are written.
+ */
+// const CommandOptions;
+
+/** Entry point for this command.
+ *
+ * @param {Array<string>} args Command-line arguments.
+ * @param {!CommandOptions} options Options.
+ * @param {function(number)} callback Callback with exit code.
+ */
+function modulenameCmd(args, options, callback) {
+  if (typeof callback !== 'function') {
+    throw new TypeError('callback must be a function');
+  }
+
+  if (args !== undefined
+      && args !== null
+      && Math.floor(args.length) !== args.length) {
+    throw new TypeError('args must be Array-like');
+  }
+
+  if (!options || typeof options !== 'object') {
+    throw new TypeError('options must be an object');
+  }
+
+  if (!options.stdin || typeof options.stdin.on !== 'function') {
+    throw new TypeError('options.stdin must be a stream.Readable');
+  }
+  if (!options.stdout || typeof options.stdout.write !== 'function') {
+    throw new TypeError('options.stdout must be a stream.Writable');
+  }
+  if (!options.stderr || typeof options.stderr.write !== 'function') {
+    throw new TypeError('options.stderr must be a stream.Writable');
+  }
+
+  if (args.length >= 2) {
+    // Strip "node" and script name, ensure args are strings
+    args = Array.prototype.slice.call(args, 2).map(String);
+  } else {
+    args = [];
+  }
+
+  const yargs = new Yargs()
+    .parserConfiguration({
+      'parse-numbers': false,
+      'parse-positional-numbers': false,
+      'dot-notation': false,
+      'duplicate-arguments-array': false,
+      'flatten-duplicate-arrays': false,
+      'greedy-arrays': false,
+      'strip-aliased': true,
+      'strip-dashed': true,
+    })
+    .usage('Usage: $0 [options] [args...]')
+    .help()
+    .alias('help', 'h')
+    .alias('help', '?')
+    .option('quiet', {
+      alias: 'q',
+      describe: 'Print less output',
+      count: true,
+    })
+    .option('verbose', {
+      alias: 'v',
+      describe: 'Print more output',
+      count: true,
+    })
+    .version(`${packageJson.name} ${packageJson.version}`)
+    .alias('version', 'V')
+    .strict();
+  yargs.parse(args, (errYargs, argOpts, output) => {
+    if (errYargs) {
+      options.stderr.write(`${output || errYargs}\n`);
+      callback(1);
+      return;
+    }
+
+    if (output) {
+      options.stdout.write(`${output}\n`);
+    }
+
+    if (argOpts.help || argOpts.version) {
+      callback(0);
+      return;
+    }
+
+    if (argOpts._.length !== 1) {
+      options.stderr.write('Error: Exactly one argument is required.\n');
+      callback(1);
+      return;
+    }
+
+    // Parse arguments then call API function with parsed options
+    const cmdOpts = {
+      files: argOpts._,
+      verbosity: argOpts.verbose - argOpts.quiet,
+    };
+    // eslint-disable-next-line promise/catch-or-return
+    modulename(cmdOpts)
+      .then(
+        () => 0,
+        (err) => {
+          options.stderr.write(`${err}\n`);
+          return 1;
+        },
+      )
+      // Note: nextTick for unhandledException (like util.callbackify)
+      .then((exitCode) => process.nextTick(callback, exitCode));
+  });
+}
+
+modulenameCmd.default = modulenameCmd;
+module.exports = modulenameCmd;
