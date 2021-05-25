b66e18679a1e834bf85b34182fb726e290f73aa1
Kevin Locke <kevin@kevinlocke.name>
2021-05-25 13:16:07 -0600

    stop parsing arguments on --version
    Behave like commander does for `.version()` and stop parsing early (by
    throwing an exception) when --version is found.  This avoids failing
    with `error: missing required argument` when `.arguments()` contains a
    required argument and the command is invoked with --version as the only
    argument.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- cli.js
+++ cli.js
@@ -72,6 +72,7 @@ export default async function modulenameMain(args, options) {
     throw new TypeError('options.stderr must be a stream.Writable');
   }
 
+  let errVersion;
   const command = new Command()
     .exitOverride()
     .configureOutput({
@@ -91,23 +92,29 @@ export default async function modulenameMain(args, options) {
     .option('-v, --verbose', 'print more output', countOption)
     // TODO: Replace with .version(packageJson.version) loaded as JSON module
     // https://github.com/nodejs/node/issues/37141
-    .option('-V, --version', 'output the version number');
+    .option('-V, --version', 'output the version number')
+    // throw exception to stop option parsing early, as commander does
+    // (e.g. to avoid failing due to missing required arguments)
+    .on('option:version', () => {
+      errVersion = new Error('version');
+      throw errVersion;
+    });
 
   try {
     command.parse(args);
   } catch (errParse) {
+    if (errVersion) {
+      const packageJson =
+        await readJson(new URL('package.json', import.meta.url));
+      options.stdout.write(`${packageJson.version}\n`);
+      return 0;
+    }
+
     // Note: Error message already printed to stderr by Commander
     return errParse.exitCode !== undefined ? errParse.exitCode : 1;
   }
 
   const argOpts = command.opts();
-
-  if (argOpts.version) {
-    const packageJson =
-      await readJson(new URL('package.json', import.meta.url));
-    options.stdout.write(`${packageJson.version}\n`);
-    return 0;
-  }
 
   const cmdOpts = {
     files: command.args,
